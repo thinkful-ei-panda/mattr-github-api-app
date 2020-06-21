992f8544971a82541b90e4d559bb44baff4a68c3
Matthew Rougely <matthewrougelysr@gmail.com>
2020-06-21 11:58:42 -0700

    js boilerplate

--- app.js
+++ app.js
@@ -1,7 +1,71 @@
-const apiKey = "";
-const searchURL = "application/vnd.github.nebula-preview+json";
-const OPTIONS = {
-    headers: new Headers({
-        "x-rapidapi-key": apiKey})
+'use strict';
+
+const apiKey = "833ea1b9d7mshbef97797dff363dp1d9ac4jsna2801a24e32d";
+
+const searchURL = 'https://api.github.com/search/topics?q=NodeJS';
+
+
+function formatQueryParams(params) {
+  const queryItems = Object.keys(params)
+    .map(key => `${encodeURIComponent(key)}=${encodeURIComponent(params[key])}`)
+  return queryItems.join('&');
+}
+
+function displayResults(responseJson, maxResults) {
+  // if there are previous results, remove them
+  console.log(responseJson);
+  $('#results-list').empty();
+  // iterate through the articles array, stopping at the max number of results
+  for (let i = 0; i < responseJson.value.length & i<maxResults ; i++){
+    // for each video object in the articles
+    //array, add a list item to the results 
+    //list with the article title, source, author,
+    //description, and image
+    $('#results-list').append(
+      `<li><h3><a href="${responseJson.value[i].url}">${responseJson.value[i].title}</a></h3>
+      <p>${responseJson.value[i].description}</p>
+      <p>By ${responseJson.value[i].body}</p>
+      </li>`
+    )};
+  //display the results section  
+  $('#results').removeClass('hidden');
 };
 
+function getNews(query, maxResults=10) {
+  const params = {
+    q: query,
+    pageSize: maxResults
+  };
+  const queryString = formatQueryParams(params)
+  const url = searchURL + '?' + queryString;
+
+  console.log(url);
+
+  const options = {
+    headers: new Headers({
+      "x-rapidapi-key": apiKey})
+  };
+
+  fetch(url, options)
+    .then(response => {
+      if (response.ok) {
+        return response.json();
+      }
+      throw new Error(response.statusText);
+    })
+    .then(responseJson => displayResults(responseJson, maxResults))
+    .catch(err => {
+      $('#js-error-message').text(`Something went wrong: ${err.message}`);
+    });
+}
+
+function watchForm() {
+  $('form').submit(event => {
+    event.preventDefault();
+    const searchTerm = $('#js-search-term').val();
+    // const maxResults = $('#js-max-results').val();
+    getNews(searchTerm, maxResults);
+  });
+}
+
+$(watchForm);
