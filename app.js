6019096e1bfce1f3996b4b6974cf831a420c61a3
Matthew Rougely <matthewrougelysr@gmail.com>
2020-06-21 12:38:22 -0700

    Finished Project

--- app.js
+++ app.js
@@ -1,34 +1,24 @@
 'use strict';
 
-const searchURL = 'https://api.github.com/users/repos';
+const searchURL = 'https://api.github.com/users/';
 
 function displayResults(responseJson) {
-  console.log(responseJson);
+//   console.log(responseJson);
   $('#results-list').empty();
-  // iterate through the articles array, stopping at the max number of results
-  for (let i = 0; i < responseJson.value.length & i<maxResults ; i++){
-    // for each video object in the articles
-    //array, add a list item to the results 
-    //list with the article title, source, author,
-    //description, and image
+  for (let i in responseJson){
+    console.log(responseJson[i])
+    
     $('#results-list').append(
-      `<li><h3><a href="${responseJson.value[i].url}">${responseJson.value[i].title}</a></h3>
-      <p>${responseJson.value[i].description}</p>
-      <p>By ${responseJson.value[i].body}</p>
+      `<li>
+      <h3>${responseJson[i].name}</h3>
+      <a href="${responseJson[i].html_url} target="blank">${responseJson[i].name}</a>
       </li>`
-    )};
-  //display the results section  
+    )}
   $('#results').removeClass('hidden');
 };
 
 function getNews(query) {
-  const params = {
-    q: query,
-    pageSize: maxResults
-  };
-  const queryString = formatQueryParams(params);
-  const url = `${searchURL}/${queryString}`;
-
+  const url = `${searchURL}${query}/repos`;
   console.log(url);
 
   const options = {
@@ -45,7 +35,7 @@ function getNews(query) {
       }
       throw new Error(response.statusText);
     })
-    .then(responseJson => displayResults(responseJson, maxResults))
+    .then(responseJson => displayResults(responseJson))
     .catch(err => {
       $('#js-error-message').text(`Something went wrong: ${err.message}`);
     });
@@ -55,8 +45,8 @@ function watchForm() {
   $('form').submit(event => {
     event.preventDefault();
     const searchTerm = $('#js-search-term').val();
-    // const maxResults = $('#js-max-results').val();
-    getNews(searchTerm, maxResults);
+    console.log(searchTerm)
+    getNews(searchTerm);
   });
 }
 
